590286dd6ac277b36abe15e61a378421912acee6
0x0is1 <42772562+0x0is1@users.noreply.github.com>
2023-08-26 01:07:23 +0530

    nothing

--- App.py
+++ App.py
@@ -1,5 +1,7 @@
 from lib.parser import SDParser
 from flask import Flask, render_template, request
+from threading import Thread
+
 
 parser = SDParser()
 app = Flask(__name__)
@@ -8,6 +10,13 @@ app = Flask(__name__)
 def home():
     show_list = parser.get_list_all(1)
     return render_template("home.html", show_list=show_list)
+
+def run():
+  app.run(host='0.0.0.0',port=8899)
+
+def wsv():  
+    t = Thread(target=run)
+    t.start()
 
 @app.route("/getshow", methods=['GET'])
 def getShow():
@@ -19,7 +28,7 @@ def fetchep():
     gep_id = request.args.get("gep_id")
     return parser.fetch_episode(gep_id)
 
-app.run(debug=True)
+wsv()
 
 # ep_id = parser.get_ep_id(season_lang_url)
 # stream_url = parser.parse_mystream(ep_id)
